3e7cd05595667484018b0b86a949180e624834d7
Marwen <marwenhammami@live.fr>
2015-04-20 18:51:58 +0100

    okjh

---
+++ src/org/primefaces/showcase/view/misc/WatermarkView.java
@@ -0,0 +1,23 @@
+package org.primefaces.showcase.view.misc;
+ 
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+ 
+@ManagedBean
+public class WatermarkView {
+     
+    private String keyword;
+ 
+    public String getKeyword() {
+        return keyword;
+    }
+ 
+    public void setKeyword(String keyword) {
+        this.keyword = keyword;
+    }
+     
+    public void search() {
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,"No results found with ", "'" + keyword + "'"));
+    }
+}
